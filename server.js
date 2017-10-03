168a040fd568bbf00aa72f8f7d2060d88ada8717
Apurva <apurva.kumar.srivastav@gmail.com>
2017-10-03 05:43:28 +0530

    working changes

--- server.js
+++ server.js
@@ -1,36 +1,39 @@
-var express=require('express');
-var app=express();
-//app.use(express.static(__dirname+"/public"));
-app.get('/fetchdata',function(req,res){
-    var  sql= require('mssql');
-    
-    var config={
-        user:'pmanshani',
-        password:'Password@123',
-        server:'localhost',
-        database:'test',
-        porrt:5000,
-        
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname+"/public"));
+app.get('/fetchdata', function (req, res) {
+    var sql = require('mssql');
+
+    var config = {
+        user: 'da',
+        password: 'test',
+        server: 'localhost\\SQLEXPRESS',
+        database: 'master',
+        port: '1433'
+
+
     };
-   
+
     console.log("i am in server");
 
-
-    sql.connect(config,function(err){
-        if(err) console.log(err);
-
-        var request=new sql.request();
-        request.query('select * from abc',function(err,record){
-            if(err) console.log(err);
-
-            res.send(record);
-
+    
+    var conn = new sql.ConnectionPool(config);
+    conn.connect().then(function () {
+        var request = new sql.Request(conn);
+        request.query("select * from abc").then(function (recordSet) {
+            console.log(recordSet);
+            conn.close();
+            res.send(recordSet);
+        }).catch(function (err) {
+            console.log(err);
+            conn.close();
+            res.send(err);
         });
-
+    }).catch(function (err) {
+        console.log(err);
     });
-
-    sql.close();
+   
 });
-var server = app.listen(5000, function () {
+var server = app.listen(3000, function () {
     console.log('Server is running..');
 });
